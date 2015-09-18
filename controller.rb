7f1ca860c71ccba3273b09fb7f0563c897f71c4c
Peter Rosal & walsh9 <pair+r05al.walsh9@devbootcamp.com>
2015-09-18 15:09:21 -0400

    Add skip and give up logic

--- controller.rb
+++ controller.rb
@@ -18,18 +18,23 @@ class Controller
   def ask_next_card(deck)
     input = ""
     current_card = deck.get_card
-    wrong_guesses = 0
     correct = false
-    until wrong_guesses > 3 || correct || input == "quit"
-      view.display_definition
+    until correct || input == "quit" || input == "skip"
+      view.display_definition(card)
       view.display_prompt
-      input = view.get_input
-      if input.downcase == current_card.term.downcase
+      input = view.get_input.downcase
+      if input == current_card.term.downcase
         view.display_correct
         correct = true
+      elsif input == "give up"
+        view.display_give_up
+        view.display_term(current_card)
+      elsif input == "skip"
+        view.display_skip
+      elsif input == "quit"
+        view.display_goodbye
       else
-        view.display_wrong
-        wrong_guesses += 1
+        display_wrong
       end
     end
     input
