847d6d022eb5fbadb6aac2860925d3ced9126dd1
Peter Rosal & walsh9 <pair+r05al.walsh9@devbootcamp.com>
2015-09-18 14:55:05 -0400

    Implement initial view logic

---
+++ view.rb
@@ -0,0 +1,41 @@
+class View
+
+  def get_input
+    gets.chomp
+  end
+
+  def diplay_prompt
+    print "What term is this describing? "
+  end
+
+  def diplay_message(arg)
+    puts arg
+  end
+
+  def diplay_definition(card)
+    puts card.definition
+  end
+
+  def diplay_term(card)
+    puts card.term
+  end
+
+  def diplay_right
+    puts "Correct! Good job!"
+  end
+
+  def diplay_wrong
+    puts "Please try again."
+  end
+
+  def diplay_hello
+    puts "Hello, welcome to Ruby Flash Cards!"
+    puts "You are able to type 'quit' to stop at anytime."
+    puts "Type 'skip' to skip the current card."
+  end
+
+  def diplay_goodbye
+    puts "Thanks for playing! Winners don't do drugs."
+  end
+
+end
