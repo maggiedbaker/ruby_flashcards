f9dff9e38dec3b4d0ce4d5517896d2d524f54939
Peter Rosal & walsh9 <pair+r05al.walsh9@devbootcamp.com>
2015-09-18 15:28:05 -0400

    Refactor to store messages in a hash

--- view.rb
+++ view.rb
@@ -1,50 +1,38 @@
 class View
 
+  MESSAGES = {
+    hello: <<-HELLO
+    Hello, welcome to Ruby Flash Cards!
+    You are able to type 'quit' to stop at anytime.
+    Type 'skip' to skip the current card.
+    Type 'give up' to see the answer.
+    HELLO
+    ,
+    goodbye: "Thanks for playing! Winners don't do drugs.",
+    right: "Correct! Good job!"
+    wrong: "Please try again."
+    skip: "Okay. Moving on..."
+    give_up: "Here is the answer: "
+  }
+
   def get_input
     gets.chomp
   end
 
-  def diplay_prompt
+  def display_prompt
     print "What term is this describing? "
   end
 
-  def diplay_message(arg)
-    puts arg
+  def display_message(message_id)
+    puts MESSAGES.fetch(message_id) {raise "Message not found"}
   end
 
-  def diplay_definition(card)
+  def display_definition(card)
     puts card.definition
   end
 
-  def diplay_term(card)
+  def display_term(card)
     puts card.term
-  end
-
-  def diplay_right
-    puts "Correct! Good job!"
-  end
-
-  def diplay_wrong
-    puts "Please try again."
-  end
-
-  def display_skip
-    puts "Okay. Moving on..."
-  end
-
-  def display_give_up
-    puts "Here is the answer: "
-  end
-
-  def diplay_hello
-    puts "Hello, welcome to Ruby Flash Cards!"
-    puts "You are able to type 'quit' to stop at anytime."
-    puts "Type 'skip' to skip the current card."
-    puts "Type 'give up' to see the answer."
-  end
-
-  def diplay_goodbye
-    puts "Thanks for playing! Winners don't do drugs."
   end
 
 end
