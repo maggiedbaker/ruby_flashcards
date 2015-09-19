6afb814b4c8121c2d9207c26bbe5ce641eb4606f
maggiedbaker <maggiedbaker@gmail.com>
2015-09-19 10:53:02 -0400

    update to allow user to input a txt file.

--- runner.rb
+++ runner.rb
@@ -5,4 +5,12 @@ require_relative 'parser'
 require_relative 'view'
 
 
-Controller.new("flashcards.txt").run
+if ARGV.first != nil
+  if ARGV.first.include? ".txt"
+    Controller.new(ARGV.first).run
+  else
+    puts "Please supply a .txt file."
+  end
+else
+  Controller.new("flashcards.txt").run
+end
