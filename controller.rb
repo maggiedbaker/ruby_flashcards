3439541bc60ba0c03fa746221e72b8864efac5bf
Phil Sofia <phil.sofia@gmail.com>
2015-09-19 22:39:50 -0400

    Update controller with mulitple choice functionality

--- controller.rb
+++ controller.rb
@@ -3,12 +3,18 @@ class Controller
   def initialize(filename)
     @view = View.new
     @deck = Deck.new(Parser.import(filename))
+    @multiple_choice = false
   end
 
   def run
     input = ""
     view.clear_screen
     view.display_message(:hello)
+    input = view.get_input
+    view.display_message(:start)
+    if input == "2"
+      @multiple_choice = true
+    end
     until input == "quit" || deck.completed
       input = ask_next_card(deck)
     end
@@ -20,10 +26,17 @@ class Controller
   def ask_next_card(deck)
     input = ""
     current_card = deck.get_card
+    if multiple_choice == true
+      # 'choices' contains three incorrect terms and the correct term, shuffled
+      choices = ((deck.get_random_incorrect_terms(current_card.term)) << current_card.term).shuffle
+    end
     correct = false
     until correct || ["quit","skip","give up"].include?(input)
       view.display_definition(current_card)
       view.display_prompt
+      if multiple_choice == true
+        view.display_choices(choices)
+      end
       input = view.get_input.downcase
       view.clear_screen
       if input == current_card.term.downcase
@@ -71,5 +84,5 @@ class Controller
     end
   end
 
-  attr_reader :view, :deck
+  attr_reader :view, :deck, :multiple_choice
 end
